394449ae2f11c0cf2b7e677ff44237005c7f501f
marcinretecki <info@marcinretecki.com>
2014-07-14 16:19:06 +0200

    Chapter 7
    Done

--- collections/posts.js
+++ collections/posts.js
@@ -1,1 +1,30 @@
 Posts = new Meteor.Collection("posts");
+
+Meteor.methods({
+  post: function(postAttributes) {
+    var user = Meteor.user(),
+        postWithSameLink = Posts.findOne({url: postAttributes.url});
+
+    if (!user) {
+      throw new Meteor.Error(401, "Musisz się zalogować Amigo.");
+    }
+
+    if (!postAttributes.title) {
+      throw new Meteor.Error(401, "Wypełnij tytuł Amigo");
+    }
+
+    if (postAttributes.url && postWithSameLink) {
+      throw new Meteor.Error(302, "Ten adres mamy już w bazie Amigo", postWithSameLink._id);
+    }
+
+    var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+      userId: user._id,
+      author: user.username,
+      submitted: new Date().getTime()
+    });
+
+    var postId = Posts.insert(post);
+
+    return postId;
+  }
+})
